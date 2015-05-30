0a5990986817d0c00c03fdd0a5ad5678249d21e4
Matteo Scandolo <matteo.scandolo@link-me.it>
2015-05-30 14:43:42 +0200

    Starting Point

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+
+var options = {
+    src: 'src/'
+};
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: options.src
+    }
+  });
+});
+
+gulp.task('serve', function() {
+  gulp.start(['browserSync']);
+  gulp.watch(options.src + 'index.html', browserSync.reload);
+});
