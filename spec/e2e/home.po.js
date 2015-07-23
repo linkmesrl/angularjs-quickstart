3824afd4ec09a5c2829b2711323e0761fa2f3057
Matteo Scandolo <matteo.scandolo@link-me.it>
2015-07-23 20:13:16 +0200

    Added async e2e test

--- spec/e2e/home.po.js
+++ spec/e2e/home.po.js
@@ -7,6 +7,13 @@
 
 var MainPage = function() {
   this.pageContent = element(by.css('.container > .row'));
+
+  this.products = element.all(by.repeater('product in products'));
+
+  this.newProductName = element(by.model('newProduct.name'));
+  this.newProductCategory = element(by.model('newProduct.category'));
+  this.newProductQuantity = element(by.model('newProduct.quantity'));
+  this.addProduct = element(by.css('.label-top'));
 };
 
 module.exports = new MainPage();
--- spec/e2e/home.test.js
+++ spec/e2e/home.test.js
@@ -12,4 +12,22 @@ describe('The home page view', function () {
     expect(page.pageContent.isPresent()).toBeTruthy();
   });
 
+  it('should should have a list of 7 elements', function() {
+    expect(page.products.count()).toEqual(7);
+  });
+
+  it('should add a product', function(done){
+
+    page.newProductCategory.sendKeys('Fruits');
+    page.newProductName.sendKeys('Mango');
+    page.newProductQuantity.sendKeys(2);
+
+    page.addProduct.click()
+    .then(function(){
+        expect(page.products.count()).toEqual(8);
+        done();
+    })
+
+  });
+
 });
